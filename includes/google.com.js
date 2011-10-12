b719ac83db95523db822d94a50c79b8161a746ed
rikaunite <jiongtl@gmail.com>
2011-10-13 02:47:10 +0800

    Patch for remove search result redirecting on Google.

---
+++ includes/google.com.js
@@ -0,0 +1,15 @@
+﻿// ==UserScript==
+// @include http://www.google.*/*
+// @include https://encrypted.google.com/*
+// ==/UserScript==
+
+(function() {
+
+window.opera.defineMagicFunction(
+  'rwt',
+  function() {
+    return true;
+  }
+);
+
+})();
