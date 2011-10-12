e1c167088e1df30fb8e1a4982dd1da9a4f244917
rikaunite <jiongtl@gmail.com>
2011-10-12 20:25:28 +0800

    First blood!

---
+++ includes/twitter.com.js
@@ -0,0 +1,16 @@
+﻿(function() {
+
+if(window.location.hostname.indexOf('twitter.com') == 0) {
+  window.document.addEventListener('DOMNodeInserted', deShortenURL, false);
+}
+
+function deShortenURL(event) {
+  var link = event.srcElement.querySelector('a[data-expanded-url]');
+
+  if(link && event.srcElement.getAttribute('class') == 'js-stream-item stream-item' || event.srcElement.getAttribute('class') == 'component') {
+    link.firstChild.data = link.href;
+    link.href = link.getAttribute('data-expanded-url');
+  }
+}
+
+})();
