8ea6f99dbfeff5ead87701b1d888ff94362f03b3
rikaunite <jiongtl@gmail.com>
2011-10-14 01:10:59 +0800

    Use "data-ultimate-url" if presented.

--- includes/twitter.com.js
+++ includes/twitter.com.js
@@ -5,19 +5,35 @@
 
 (function() {
 
-window.document.addEventListener('DOMNodeInserted', deShortenURL, false);
+window.document.addEventListener('DOMNodeInserted', replaceURL, false);
 
-function deShortenURL(event) {
+function replaceURL(event) {
   var links = event.srcElement.querySelectorAll('a[data-expanded-url]');
   var nodeClass = event.srcElement.getAttribute('class');
 
-  if(links && nodeClass == 'js-stream-item stream-item' || nodeClass == 'component') {
+  if(links.length > 0 && (nodeClass == 'js-stream-item stream-item' || nodeClass == 'component')) {
     for(var i = 0; i < links.length; i++) {
       var link = links[i];
 
+      link.addEventListener('DOMAttrModified', replaceToUltimateURL, false);
       link.firstChild.data = link.href;
       link.href = link.getAttribute('data-expanded-url');
     }
+  }
+}
+
+function replaceToUltimateURL(event) {
+  if(event.attrName == 'data-ultimate-url') {
+    var link = event.srcElement;
+    var url = link.getAttribute(event.attrName);
+
+    if(url[url.length-1] == '/') {
+      link.href = url.substring(0, url.length-1);
+    } else {
+      link.href = url;
+    }
+
+    link.removeEventListener('DOMAttrModified', replaceToUltimateURL, false);
   }
 }
 
