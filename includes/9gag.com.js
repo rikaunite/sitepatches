d9a0e2d1309e159cd623317f956ca912641c8c5c
rikaunite <jiongtl@gmail.com>
2012-02-19 20:10:24 +0800

    Bypass 9gag NSFW mask without login.

---
+++ includes/9gag.com.js
@@ -0,0 +1,25 @@
+﻿// ==UserScript==
+// @include http://9gag.com/*
+// ==/UserScript==
+
+(function() {
+
+window.document.addEventListener('DOMContentLoaded', bypass_nsfw_block, false);
+window.document.addEventListener('DOMNodeInserted', bypass_nsfw_block, false);
+
+function bypass_nsfw_block(event) {
+  var imgs = document.getElementsByTagName("img");
+
+  for(i = 0; i < imgs.length; i++) {
+    var img = imgs[i];
+
+    if(img.getAttribute("alt") == "NSFW") {
+      var img_id = img.parentNode.getAttribute("href").split("/")[2];
+
+      img.src = "http://d24w6bsrhbeh9d.cloudfront.net/photo/" + img_id + "_460s.jpg";
+      img.removeAttribute("alt");
+    }
+  }
+}
+
+})();
