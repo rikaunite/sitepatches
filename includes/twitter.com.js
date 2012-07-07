afcc77e42e5659a0a179c2069ab2b1b6f8c69a84
rikaunite <jiongtl@gmail.com>
2012-07-07 14:01:42 +0800

    Fix Twitter URL expander.

--- includes/twitter.com.js
+++ includes/twitter.com.js
@@ -7,7 +7,26 @@ var DISPLAY_EXPANDED_URL = false;
 
 (function() {
 
+window.document.addEventListener('DOMContentLoaded', replace_url_loaded, false);
 window.document.addEventListener('DOMNodeInserted', replace_url, false);
+
+function replace_url_common(link) {
+  link.querySelector('span.js-display-url').firstChild.data = DISPLAY_EXPANDED_URL ? link.getAttribute('data-expanded-url') : link.href;
+  link.removeChild(link.querySelector('span.tco-ellipsis'));
+  link.href = link.getAttribute('data-expanded-url');
+}
+
+function replace_url_loaded(event) {
+  var links = event.srcElement.querySelectorAll('a[data-expanded-url]');
+
+  if(links.length > 0) {
+    for(var i = 0; i < links.length; i++) {
+      var link = links[i];
+
+      replace_url_common(link);
+    }
+  }
+}
 
 function replace_url(event) {
   var links = event.srcElement.querySelectorAll('a[data-expanded-url]');
@@ -16,9 +35,17 @@ function replace_url(event) {
     for(var i = 0; i < links.length; i++) {
       var link = links[i];
 
-      link.addEventListener('DOMAttrModified', replace_url_ultimate, false);
-      link.firstChild.data = DISPLAY_EXPANDED_URL ? link.getAttribute('data-expanded-url') : link.href;
-      link.href = link.getAttribute('data-expanded-url');
+      if(link.querySelectorAll('span').length > 0) {
+        replace_url_common(link);
+      } else if(event.srcElement.getAttribute('class').search(/expanding-stream-item/)) {
+        if(link.href != link.getAttribute('data-expanded-url')) {
+          if(!link.getAttribute('data-ultimate-url')) {
+            link.addEventListener('DOMAttrModified', replace_url_ultimate, false);
+            link.firstChild.data = DISPLAY_EXPANDED_URL ? link.getAttribute('data-expanded-url') : link.href;
+            link.href = link.getAttribute('data-expanded-url');
+          }
+        }
+      }
     }
   }
 }
